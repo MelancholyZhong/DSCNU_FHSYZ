f4458b8bc1256c6cdedc3472018964c78c6ef1fb
Yao Zhong <a26250@baobeiben.local>
2022-12-03 14:09:47 -0800

    QRCode

--- server/service/info-service.js
+++ server/service/info-service.js
@@ -3,7 +3,13 @@ const { db_insertInfo, db_getInfoById } = require("../db_controller/db_info");
 const addInfo = async (req, res) => {
   const contactInfo = req.body;
   const id = await db_insertInfo(contactInfo);
-  res.status(200).json({ id: id, info: contactInfo });
+  const QRCode = getQRCode(id);
+  res.status(200).json({ id: id, info: contactInfo, QRCode: QRCode });
+};
+
+const getQRCode = (id) => {
+  const prefix = "http://localhost:3001/api/info/";
+  return `https://api.qrserver.com/v1/create-qr-code/?data=${prefix + id}"`;
 };
 
 const getInfo = async (req, res) => {
