b10026082f7d78ab7c1c2895ca45e0d5a562502e
Yao Zhong <a26250@baobeiben.hsd1.ca.comcast.net>
2022-12-04 07:46:27 -0800

    added eamil things

--- server/service/email-service.js
+++ server/service/email-service.js
@@ -1,56 +1,57 @@
-// const fs = require('fs');
-// const path = require('path');
-// const {google} = require('googleapis');
-// const MailComposer = require('nodemailer/lib/mail-composer');
-// const key = require(path.join(__dirname, 'hackathon-email-370609-f523e162e57c'));
+const fs = require("fs");
+const path = require("path");
+const { google } = require("googleapis");
+const MailComposer = require("nodemailer/lib/mail-composer");
+const key = require(path.join(__dirname, "../email-cred.json"));
 
-// const SCOPES = [
-//   'https://mail.google.com/',
-//   'https://www.googleapis.com/auth/gmail.addons.current.action.compose',
-//   'https://www.googleapis.com/auth/gmail.readonly',
-//   'https://www.googleapis.com/auth/gmail.send',
-//   'https://www.googleapis.com/auth/gmail.modify',
-//   'https://www.googleapis.com/auth/gmail.compose'];
-// const SENDER_EMAIL = 'admin@fshyz.com';
+const SCOPES = [
+  "https://mail.google.com/",
+  "https://www.googleapis.com/auth/gmail.addons.current.action.compose",
+  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/gmail.send",
+  "https://www.googleapis.com/auth/gmail.modify",
+  "https://www.googleapis.com/auth/gmail.compose",
+];
+const SENDER_EMAIL = "admin@fshyz.com";
 
-// const gmail = () => {
-//   const JWT = google.auth.JWT;
-//   const authClient = new JWT({
-//     email: key.client_email,
-//     key: key.private_key,
-//     scopes: SCOPES,
-//     subject: SENDER_EMAIL,
-//   });
-//   return google.gmail({version: 'v1', auth: authClient});
-// }
+const gmailClient = () => {
+  const JWT = google.auth.JWT;
+  const authClient = new JWT({
+    email: key.client_email,
+    key: key.private_key,
+    scopes: SCOPES,
+    subject: SENDER_EMAIL,
+  });
+  return google.gmail({ version: "v1", auth: authClient });
+};
 
-// const encodeMessage = (message) => {
-//   return Buffer.from(message).toString('base64');
-// }
+const encodeMessage = (message) => {
+  return Buffer.from(message).toString("base64");
+};
 
-// const createMail = async (options) => {
-//   const composer = new MailComposer(options);
-//   const message = await composer.compile().build();
-//   return encodeMessage(message);
-// }
+const createMail = async (options) => {
+  const composer = new MailComposer(options);
+  const message = await composer.compile().build();
+  return encodeMessage(message);
+};
 
-// const sendFirstEmail = async (info) => {
-//   const gmail = gmail();
-//   const messageOptions = {
-//     subject: 'Your emergency code is scanned!',
-//     textEncoding: 'base64',
-//   }
-//   messageOptions.from = SENDER_EMAIL;
-//   messageOptions.to = info.email;
-//   messageOptions.text = info.message_content;
-//   const rawMessage = await createMail(options);
-//   const res = await gmail.users.messages.send({
-//     userId: 'me',
-//     requestBody: {
-//       "raw": rawMessage,
-//     },
-//   });
-//   console.log(res.data);
-// };
+const sendFirstEmail = async (info) => {
+  const client = gmailClient();
+  const messageOptions = {
+    subject: "Your emergency code is scanned!",
+    textEncoding: "base64",
+  };
+  messageOptions.from = SENDER_EMAIL;
+  messageOptions.to = info.email;
+  messageOptions.text = info.message_content;
+  const rawMessage = await createMail(messageOptions);
+  const res = await client.users.messages.send({
+    userId: "me",
+    requestBody: {
+      raw: rawMessage,
+    },
+  });
+  console.log(res.data);
+};
 
-// module.exports = { sendFirstEmail };
+module.exports = { sendFirstEmail };
